ce6b128b300bea3272ac324e53277f28f5e41364
7Kaika7 <146513055+7Kaika7@users.noreply.github.com>
2024-04-30 00:16:32 +0400

    Minus

--- Class.cs
+++ Class.cs
@@ -10,5 +10,7 @@ namespace Пр3_ИСРПО_
     {
         public double Plus(double a, double b)
         { return a + b; }
+        public double Minus(double a, double b)
+        { return a - b; }
     }
 }
