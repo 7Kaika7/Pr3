53a8fb2c2b7b2fb101f867344a5213d5142a356c
7Kaika7 <146513055+7Kaika7@users.noreply.github.com>
2024-04-30 00:09:05 +0400

    Plus

---
+++ Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пр3_ИСРПО_
+{
+    internal class Class
+    {
+        public double Plus(double a, double b)
+        { return a + b; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+using Пр3_ИСРПО_;
+
+namespace pr
+{
+   internal class Program
+   {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите А:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите операцию:");
+            char c = Convert.ToChar(Console.ReadLine());
+            Class clas = new Class();
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine($"a + b = {clas.Plus(a,b)}");
+                    break;
+            }
+            Console.ReadKey(); 
+        }
+   }
+}
